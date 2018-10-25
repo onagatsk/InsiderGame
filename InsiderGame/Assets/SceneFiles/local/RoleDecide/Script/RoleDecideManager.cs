c45f760a9474b6393a6d3a53b620b5ded02a2c79
DESKTOP-CLO51OL\翁長駿圭 <toshiki-o-16d@stu.kbc.ac.jp>
2018-10-25 14:45:16 +0900

    ジョブの割り振りと表示まで完了

---
+++ InsiderGame/Assets/SceneFiles/local/RoleDecide/Script/RoleDecideManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class RoleDecideManager : MonoBehaviour
+{
+    private static RoleDecideManager Singleton = null;
+
+    static RoleDecideManager instance
+    {
+        get { return Singleton ?? (Singleton = FindObjectOfType<RoleDecideManager>()); }
+    }
+
+    public enum Role
+    {
+        Common,
+        Master,
+        Insider
+    }
+
+    [SerializeField]
+    public static Role[] RoleNumber;
+
+    void Start()
+    {
+        if (this != instance)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        RoleNumber = new Role[NumberInputFieldManager.IGameMenber];
+        //マスターを決定
+        setJpb(Role.Master, Role.Insider);
+        //インサイダーを決定
+        setJpb(Role.Insider, Role.Master);
+    }
+    void OnStart()
+    {
+       
+    }
+    void Update()
+    {
+        OnStart();
+    }
+
+    //第一引数がセットしたいジョブ、第二引数が避けたいジョブ
+    void setJpb(Role SetJob, Role AvoidJob)
+    {
+        for (int Maseter = UnityEngine.Random.Range(0, NumberInputFieldManager.IGameMenber);
+            RoleNumber[Maseter] != SetJob;)
+        {
+            if (RoleNumber[Maseter] != AvoidJob)
+            {
+                RoleNumber[Maseter] = SetJob;
+            }
+            else
+            {
+                Maseter = UnityEngine.Random.Range(0, NumberInputFieldManager.IGameMenber);
+            }
+        }
+    }
+}
